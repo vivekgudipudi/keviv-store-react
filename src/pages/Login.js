e04d55cd1b98a85aed3c1af97130affd786c5098
Vivek Gudipudi <37794298+vivekgudipudi@users.noreply.github.com>
2022-06-23 12:15:08 +0530

    feat : added guest login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -40,11 +40,16 @@ export const Login = ()=> {
                 <div className="heading-sign-in t3 bold">LOGIN</div><hr/>
                 <div className="sub-heading-sign-in light">Enter your details.</div><hr/>
                 <div className="input-box-sign-in">
-                    <input type="email" placeholder="Email"  onChange={(e)=>setCredentials((a)=>({...a, email: e.target.value}))} required />
-                    <input type="password" placeholder="Password" onChange={(e)=>setCredentials((a)=>({...a, password: e.target.value}))} required />
+                    <input type="email" placeholder="Email"  onChange={(e)=>setCredentials((a)=>({...a, email: e.target.value}))}/>
+                    <input type="password" placeholder="Password" onChange={(e)=>setCredentials((a)=>({...a, password: e.target.value}))}/>
                 </div>
                 <div className="btn-box-sign-in">
                     <input type = "submit" value = "LOG IN" className="btn btn-sign-in"/>
+                </div><br/>
+                <div className="btn-box-sign-in">
+                    <input type = "submit" value = "SIGN IN AS GUEST" onClick={(e)=> {
+                        setCredentials((a)=>({...a,email : "vivekgudipudi@gmail.com",password : "vivekgudipudi"}))
+                    }} className="btn btn-sign-in"/>
                 </div>
                 <div className="login-text"> 
                 <NavLink to="/signup">Don't have an account?<span className="login-text" >Create here..</span>
